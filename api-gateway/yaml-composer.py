547397dede62d962b7919f78c7dba785e423aa11
heavensward.argentlux <heavensward.argentlux@gmail.com>
2021-05-11 14:47:38 +0800

    updated api-gateway, created yaml composer

---
+++ api-gateway/yaml-composer.py
@@ -0,0 +1,38 @@
+import yaml
+
+def compose(out_file, base_file, paths_files):
+	base = load_base(base_file)
+	base['paths'] = {}
+	for file in paths_files:
+		with open(file, 'r') as stream:
+			try:
+				paths = yaml.safe_load(stream)['paths'] # {'/policies' : {...}, '/policies/{policyId}' : {...} }
+				for path in paths.keys():
+					base['paths'][path] = paths[path]			# assign above ^ to {'paths' : {...} }
+			except yaml.YAMLError as e: print(e)
+		stream.close()
+	status = write_yaml(out_file, base)
+	return status, base
+
+def load_base(base_file):
+	with open(base_file, 'r') as stream:
+		try: base = yaml.safe_load(stream)
+		except yaml.YAMLError as e: print(e)
+	stream.close()
+	return base
+
+def write_yaml(out_file, data):
+	with open(out_file, 'w') as stream:
+		try: yaml.safe_dump(data, stream)
+		except yaml.YAMLError as e: print(e)
+	stream.close()
+	return 'OK'
+
+if __name__ == "__main__":
+	base_file = 'gateway-v3-base.yaml'
+	path_files = [
+		'paths/policy/gateway-v3-policies.yaml',
+		'paths/policy/gateway-v3-expenses.yaml'
+	]
+	status, _ = compose("gateway-v3.1.yaml", base_file, path_files)
+	print(status)
